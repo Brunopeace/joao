89ce3e63110933e24ea5ee0d6c39eab8deb13574
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2024-08-07 22:54:50 -0300

    Atualizar o script.js

--- javascript/script.js
+++ javascript/script.js
@@ -52,13 +52,9 @@ document.getElementById('backToTop').onclick = function() {
 };
 
 
-
-
-
-
-
 function verificarAcesso() {
-    const uuidEsperado = ['d0709af9-0c05-4f56-8808-30f18efa7f86'];
+    const uuidEsperado = ['d0709af9-0c05-4f56-8808-30f18efa7f86',
+ 'bebd18af-b85d-48f5-a651-e73c084da800];
     let uuidArmazenado = localStorage.getItem('uuid');
 
     if (!uuidArmazenado) {
